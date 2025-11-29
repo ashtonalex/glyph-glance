d83fdb52db2403fc7c697973fac2b6d23388be60
ashtonalex <ashtongregoryalexander@gmail.com>
2025-11-29 02:08:46 +0000

    new

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,6 +35,7 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.androidx.datastore.preferences)
+
             implementation(libs.kotlinx.coroutines.android)
             implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar", "*.jar"))))
         }
